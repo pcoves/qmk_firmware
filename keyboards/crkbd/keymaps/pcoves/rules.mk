c466e14ef975fafe1495b23decd051dfd712c367
Pablo COVES <pablo.coves@protonmail.com>
2020-06-18 19:46:07 +0200

    Business as usual

--- keyboards/crkbd/keymaps/pcoves/rules.mk
+++ keyboards/crkbd/keymaps/pcoves/rules.mk
@@ -1,10 +1,4 @@
-
-# If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/glcdfont.c \
-        ./lib/rgb_state_reader.c \
-        ./lib/layer_state_reader.c \
-        ./lib/logo_reader.c \
-        ./lib/keylogger.c \
-        # ./lib/mode_icon_reader.c \
-        # ./lib/host_led_state_reader.c \
-        # ./lib/timelogger.c \
+COMBO_ENABLE           = yes
+RAINBOW_UNICORN_ENABLE = no
+TAP_DANCE_ENABLE       = yes
+UNICODE_ENABLE         = yes
